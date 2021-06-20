057837905cbbb786757170551b3f36ab452b9992
aKoslov <alejoksv@protonmail.com>
2021-06-20 17:03:56 -0300

    Fixed DB Schema Reference

--- Tienda.Database/dbo/Stored Procedures/GetUsersList.sql
+++ Tienda.Database/dbo/Stored Procedures/GetUsersList.sql
@@ -3,5 +3,5 @@ CREATE     PROCEDURE [dbo].GetUsersList
 AS 
 	BEGIN
 	SET NOCOUNT ON
-	SELECT UI.DNI, UI.Name, UI.LastName, UI.PhoneNumber, UI.Username, UI.CreationDate FROM UserInfo UI
+	SELECT UI.DNI, UI.Name, UI.LastName, U.PhoneNumber, UI.Username, UI.CreationDate FROM UsersInfo UI JOIN Users U ON U.Username = UI.Username
 	END
