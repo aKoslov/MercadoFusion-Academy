b5ca5e272a7b419d8ab3800f2d792e958b9cf5f5
aKoslov <alejoksv@protonmail.com>
2021-06-29 19:44:50 -0300

    usted no aprende verdad

--- 1 - WebApp/src/app/services/category.service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Category.Service } from './category.service';
-
-describe('Category.Service', () => {
-  it('should create an instance', () => {
-    expect(new Category.Service()).toBeTruthy();
-  });
-});
