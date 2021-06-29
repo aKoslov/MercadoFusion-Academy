83f37bfe0b86800a251ee4f1a3e29c41a550580d
aKoslov <alejoksv@protonmail.com>
2021-06-29 18:32:47 -0300

    SCSS briefly changed to CSS due to incompatibility

--- 1 - WebApp/src/app/components/shopping/shopping.component.ts
+++ 1 - WebApp/src/app/components/shopping/shopping.component.ts
@@ -3,12 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
-  styleUrls: ['./shopping.component.scss']
+  styleUrls: ['./shopping.component.css']
 })
 export class ShoppingComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
 
+  }
+  
   ngOnInit(): void {
   }
 
